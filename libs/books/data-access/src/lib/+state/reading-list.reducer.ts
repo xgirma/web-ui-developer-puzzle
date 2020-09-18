ec3ea1e79bce2440eb24249c6ca98fba5631bfda
Girma Nigusse <xgirma@gmail.com>
2020-09-17 21:54:33 -0400

    Undo add and remove from reading list

--- libs/books/data-access/src/lib/+state/reading-list.reducer.ts
+++ libs/books/data-access/src/lib/+state/reading-list.reducer.ts
@@ -53,10 +53,13 @@ const readingListReducer = createReducer(
   on(ReadingListActions.removeFromReadingList, (state, action) =>
     readingListAdapter.removeOne(action.item.bookId, state)
   ),
-  on(ReadingListActions.failedRemoveFromReadingList, (state, action) =>
+  on(
+    ReadingListActions.failedRemoveFromReadingList,
+    ReadingListActions.undoRemoveFromReadingList,(state, action) =>
     readingListAdapter.addOne(action.item, state)
   ),
-  on(ReadingListActions.failedAddToReadingList, (state, action) =>
+  on(ReadingListActions.failedAddToReadingList,
+    ReadingListActions.undoAddToReadingList,(state, action) =>
     readingListAdapter.removeOne(action.book.id, state)
   )
 );
